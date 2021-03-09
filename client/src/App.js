55aa1b48b83fa8e0241b92d8208fc705c227320b
Nhi Danis <a.nhi1001@gmail.com>
2021-03-08 21:17:30 -0800

    add jumbotron

--- client/src/App.js
+++ client/src/App.js
@@ -1,12 +1,24 @@
-
+import React, { Component } from "react";
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
+import Home from "./pages/home";
+import Saved from "./pages/saved"
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      
-    </div>
-  );
+class App extends Component {
+  render() {
+    return (
+      <Router>
+      <div>
+        <Switch>
+          <Route exact path="/" component={Home} />
+          <Route exact path="/saved" component={Saved} />
+        </Switch>
+      </div>
+      </Router>
+    )
+
+  }
+  
 }
 
 export default App;
